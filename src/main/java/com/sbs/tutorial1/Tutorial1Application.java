c4028294735b25cbb819702c8f9b10ff9f937f5a
Moonyoungsik95 <moonyoungsik95@gmail.com>
2025-03-24 11:30:19 +0900

    home/main1, home/main2, home/main3 반영

--- src/main/java/com/sbs/tutorial1/Tutorial1Application.java
+++ src/main/java/com/sbs/tutorial1/Tutorial1Application.java
@@ -2,6 +2,7 @@ package com.sbs.tutorial1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class Tutorial1Application {
---
+++ src/main/java/com/sbs/tutorial1/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.sbs.tutorial1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home/main1")
+    @ResponseBody
+    public String showHome() {
+        return "어서오세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showHome2(){
+        return "환영합니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showHome3(){
+        return "반갑습니다.";
+    }
+}
